57bb5025e7061aacf6efd51b7514b6d998bd5d05
Paul Bodner <bodner80@gmail.com>
2022-01-19 16:16:59 -0800

    include messages to snake game

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,6 +15,43 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  else if (key === 'w') {
+     connection.write("Move: up")
+    //console.log(connection);
+  }
+  
+  else if (key === 'd') {
+    connection.write("Move: right")
+   //console.log(connection);
+  }
+
+  else if (key === 's') {
+    connection.write("Move: down")
+   //console.log(connection);
+  }
+
+  else if (key === 'a') {
+    connection.write("Move: left")
+ //console.log(connection);
+  }
+
+  else if (key === 'b') {
+    connection.write("Say: Thats what he said")
+  }
+
+  else if (key === 'g') {
+    connection.write("Say: Thats what she said")
+  }
+
+  else if (key === 'n') {
+    connection.write("Say: Nope!")
+  }
+
+  else if (key === '1') {
+    connection.write("Say: I win")
+  }
+
 };
 
 module.exports = {
--- play.js
+++ play.js
@@ -11,6 +11,6 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
+const connection = connect();
 
-setupInput();
+setupInput(connection);
